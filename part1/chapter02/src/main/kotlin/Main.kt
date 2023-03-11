85dd422a21a644faad10a1c316602f67df7de454
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2023-03-11 09:08:46 +0900

    :sparkles: p41のgetter,setterまで実装完了

--- part1/chapter02/src/main/kotlin/Main.kt
+++ part1/chapter02/src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 fun main() {
-  printOddOrEvenNumberText(3)
-  printNumText(100)
+  user()
 }
 
 /**
@@ -54,4 +53,96 @@ fun printNumText(num: Int) {
     else -> "undefined value"
   }
   println(text)
+}
+
+/**
+ * プロパティの定義でgetterとsetterが不要になる
+ * Javaではlombokの@Getterや@Setterなどでコードを省略していた
+ *
+ */
+class User1 {
+  /**
+   * 名前
+   * <code>var<code>で定義するとgetterとsetterどちらも内部的に生成されている
+   */
+  var name: String = ""
+}
+
+/**
+ * valで定義するとgetterだけ生成される
+ * そのためインスタンス生成以降、変更させないようにするためにはプロパティをvalで定義する
+ *
+ * @param id ID
+ */
+class User2(id: Int) {
+  val id:Int = id
+  var name: String = ""
+}
+
+/**
+ * lateinit
+ *
+ * val, varによるプロパティ定義では必ず初期値を設定する必要がある
+ * しかし場合によってはインスタンス生成後に値を設定したいこともある
+ *
+ * そのようなときはlateinitと呼ばれる遅延初期化プロパティにする
+ * lateinitはインスタンス生成後に値を書き換えるためvarでのみ使える
+ * 後から必ず値を設定する必要があるため設定する前にgetterを呼び出すと実行時エラーになる
+ *
+ */
+class User3 {
+  lateinit var name: String
+}
+
+/**
+ * 拡張プロパティでgetter, setterの処理を拡張する
+ *
+ * 内部的に生成していたgetter, setterを拡張して独自処理を書ける
+ *
+ */
+class User4 {
+  /**
+   * nameのsetterを拡張
+   * 空文字なら"Kotlin"そうでないなら設定されている値
+   */
+  var name: String = ""
+    set(value) {
+      field = if (value == "") {
+        "Kotlin"
+      } else {
+        value
+      }
+    }
+
+  /**
+   * isValidNameというプロパティのgetterを拡張
+   * User4のインスタンスを生成してからisValidNameを呼び出すとget()が呼ばれる
+   * 名前が空文字ではないかを判定する
+   */
+  val isValidName: Boolean
+    get() = name != ""
+}
+
+fun user(){
+  val user = User1()
+  user.name = "Test"
+  println(user.name)
+
+  val user2 = User2(100)
+  user2.name = "Test2"
+  println(user2.id)
+
+  val user3 = User3()
+  // kotlin.UninitializedPropertyAccessException: lateinit property name has not been initialized
+  //  println(user3.name) 実行時にエラー
+
+  user3.name = "Test3"
+  println(user.name)
+
+  val user4 = User4()
+  user4.name = ""
+  println(user4.name) // Kotlin
+  user4.name = "Test4"
+  println(user4.name) // Test4
+  println(user4.isValidName)
 }
