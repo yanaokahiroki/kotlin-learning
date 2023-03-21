367d68c36db23bb067f97c80f44bc2bdef6e17f3
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2023-03-21 22:07:29 +0900

    :sparkles: p69のデリゲートまで実装完了

--- part1/chapter02/src/main/kotlin/Main.kt
+++ part1/chapter02/src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
+import java.lang.IllegalStateException
+
 fun main() {
-  operatorExample()
+  val executor = AddCalculationExecutorDelegate(CommonCalculationExecutor())
+  executor.printStartMessage()
+  println(executor.calc(8, 11))
 }
 
 /**
@@ -455,4 +459,42 @@ fun operatorExample() {
   val lessThan = Num(5) < Num(1)
   println(greaterThan)
   println(lessThan)
+}
+
+interface CalculationExecutor {
+  val message: String
+
+  fun calc(num1: Int, num2: Int): Int
+  fun printStartMessage()
+}
+
+class CommonCalculationExecutor(override val message: String = "calc") : CalculationExecutor {
+  override fun calc(num1: Int, num2: Int): Int {
+    throw IllegalStateException("Not implements calc")
+  }
+
+  override fun printStartMessage() {
+    println("start $message")
+  }
+}
+
+class AddCalculationExecutor(private val calculationExecutor: CalculationExecutor) : CalculationExecutor {
+
+  override val message: String
+    get() = calculationExecutor.message
+
+  override fun calc(num1: Int, num2: Int): Int {
+    return num1 + num2
+  }
+
+  override fun printStartMessage() {
+    calculationExecutor.printStartMessage()
+  }
+}
+
+class AddCalculationExecutorDelegate(private val calculationExecutor: CalculationExecutor) :
+  CalculationExecutor by calculationExecutor {
+  override fun calc(num1: Int, num2: Int): Int {
+    return num1 + num2
+  }
 }
