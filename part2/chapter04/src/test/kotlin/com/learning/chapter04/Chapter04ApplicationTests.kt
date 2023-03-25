dd8596d67135fbf5292be5f824933c1bab96d126
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2023-03-25 19:59:47 +0900

    :sparkles: p105のSpring Bootの導入まで実装完了

---
+++ part2/chapter04/src/test/kotlin/com/learning/chapter04/Chapter04ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learning.chapter04
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter04ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
